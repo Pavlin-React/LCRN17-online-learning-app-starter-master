98f1605def9d0a775e46bb422303cb26428be637
Pavlin Ivanov <pavlinivanov9@gmail.com>
2022-01-29 12:09:43 +0100

    bottomTab

--- screens/Dashboard/MainLayout.js
+++ screens/Dashboard/MainLayout.js
@@ -1,4 +1,4 @@
-import React, {useRef} from 'react';
+import React, {useRef, createRef} from 'react';
 import {
   View,
   Text,
@@ -9,6 +9,50 @@ import {
 } from 'react-native';
 import {Home, Profile, Search} from '../../screens';
 import {constants, COLORS, FONTS, SIZES} from '../../constants';
+import {Shadow} from 'react-native-shadow-2';
+
+let bottom_tabs = constants.bottom_tabs.map(bottom_tab => ({
+  ...bottom_tab,
+  ref: createRef(),
+}));
+
+let Tabs = ({scrollX}) => {
+  return (
+    <View style={{flex: 1, flexDirection: 'row'}}>
+      {/* Tabs */}
+      {bottom_tabs.map((item, index) => {
+        return (
+          <TouchableOpacity
+            key={`Bottom-Tab${index}`}
+            ref={item.ref}
+            style={{
+              flex: 1,
+              paddingHorizontal: 15,
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}>
+            <Image
+              source={item.icon}
+              resizeMode="contain"
+              style={{
+                width: 25,
+                height: 25,
+              }}
+            />
+            <Text
+              style={{
+                marginTop: 3,
+                color: COLORS.white,
+                ...FONTS.h3,
+              }}>
+              {item.label}
+            </Text>
+          </TouchableOpacity>
+        );
+      })}
+    </View>
+  );
+};
 
 const MainLayout = () => {
   let flatList = useRef();
@@ -20,7 +64,7 @@ const MainLayout = () => {
           ref={flatList}
           pagingEnabled
           horizontal
-          snapToAlignment='center'
+          snapToAlignment="center"
           snapToInterval={SIZES.width}
           decelerationRate="fast"
           showHorizontalScrollIndicator={false}
@@ -35,13 +79,34 @@ const MainLayout = () => {
           renderItem={({item, index}) => {
             return (
               <View style={{width: SIZES.width, height: SIZES.height}}>
-               {item.label == constants.screens.home && <Home/>}
-               {item.label == constants.screens.search && <Search/>}
-               {item.label == constants.screens.profile && <Profile/>}
+                {item.label == constants.screens.home && <Home />}
+                {item.label == constants.screens.search && <Search />}
+                {item.label == constants.screens.profile && <Profile />}
               </View>
             );
           }}
         />
+      </View>
+    );
+  };
+  let renderBottomTab = () => {
+    return (
+      <View
+        style={{
+          marginBottom: 20,
+          paddingHorizontal: SIZES.padding,
+          paddingVertical: SIZES.radius,
+        }}>
+        <Shadow size={[SIZES.width - SIZES.padding * 2, 85]}>
+          <View
+            style={{
+              flex: 1,
+              borderRadius: SIZES.radius,
+              backgroundColor: COLORS.primary3,
+            }}>
+            <Tabs scrollX={scrollX} />
+          </View>
+        </Shadow>
       </View>
     );
   };
@@ -50,6 +115,7 @@ const MainLayout = () => {
       {/* Content */}
       {renderContent()}
       {/* Bottom Tab */}
+      {renderBottomTab()}
     </View>
   );
 };
